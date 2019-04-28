b69692b5e66ad4acd1091b7b4769947a1390b70b
Blake Watson <blake@blakewatson.com>
2019-04-27 23:33:02 -0500

    added basic sorting

--- src/components/DocTable.vue
+++ src/components/DocTable.vue
@@ -7,18 +7,20 @@
             <table class="table is-fullwidth">
                 <thead>
                     <tr class="is-size-7">
-                        <th class="doctable-title-col">Title</th>
-                        <th>Description</th>
+                        <SortToggle text="ID" field="id" :sortBy="sortBy" :sort="sort" @sortcolumn="sortColumn" />
+                        <SortToggle class="doctable-title-col" text="Title" field="name" :sortBy="sortBy" :sort="sort" @sortcolumn="sortColumn" />
+                        <SortToggle text="Description" field="description" :sortBy="sortBy" :sort="sort" @sortcolumn="sortColumn" />
                         <th>Tags</th>
-                        <th>Last viewed</th>
-                        <th>Expiration</th>
+                        <SortToggle text="Last viewed" field="lastViewed" :sortBy="sortBy" :sort="sort" @sortcolumn="sortColumn" />
+                        <SortToggle text="Expiration" field="expirationDate" :sortBy="sortBy" :sort="sort" @sortcolumn="sortColumn" />
                     </tr>
                 </thead>
                 <tbody>
                     <tr v-if="documents.length === 0">
                         <td colspan="5">No documents match your search.</td>
                     </tr>
-                    <tr v-for="doc in documents" :key="doc.id" v-if="documents.length">
+                    <tr v-for="doc in sortedDocuments" :key="doc.id" v-if="documents.length">
+                        <td>{{ doc.id }}</td>
                         <td><router-link :to="{ name: 'doc', params: { id: doc.id } }">{{ doc.name }}</router-link></td>
                         <td>{{ doc.description }}</td>
                         <td>
@@ -37,9 +39,60 @@
 
 <script lang="ts">
 import Vue from 'vue';
+import SortToggle from '@/components/SortToggle.vue';
+import { Documint } from '../types';
 
 export default Vue.extend({
     name: 'DocTable',
     props: ['documents'],
+    data() {
+        return {
+            sortBy: 'id',
+            sort: 'ASC'
+        }
+    },
+    computed: {
+        sortedDocuments(): Documint[] {
+            let sortedDocs: Documint[] = [...this.documents];
+            sortedDocs.sort((a: Documint, b: Documint): number => {
+                if(
+                    this.sortBy === 'id' ||
+                    this.sortBy === 'name' ||
+                    this.sortBy === 'description' ||
+                    this.sortBy === 'lastViewed' ||
+                    this.sortBy === 'expirationDate'
+                ) {
+                    if(a[this.sortBy] < b[this.sortBy]) return -1;
+                    if(a[this.sortBy] > b[this.sortBy]) return 1;
+                }
+                return 0;
+            });
+            if(this.sort === 'DESC') {
+                sortedDocs.reverse();
+            }
+            return sortedDocs;
+        }
+    },
+    methods: {
+        sortColumn(field: string) {
+            if(this.sortBy === field) {
+                this.toggleSortOrder();
+            } else {
+                this.sortBy = field;
+                this.sort = 'ASC';
+            }
+        },
+
+        toggleSortOrder() {
+            if(this.sort === 'ASC') {
+                this.sort = 'DESC';
+            } else {
+                this.sort = 'ASC';
+            }
+        }
+    },
+    components: {
+        SortToggle
+    }
 });
 </script>
---
+++ src/components/SortToggle.vue
@@ -0,0 +1,30 @@
+<template>
+    <th class="sort-toggle" @click="sortColumn">
+        {{ text }}&nbsp;&nbsp;<i class="fas" v-if="sortBy === field" :class="iconClasses"></i>
+    </th>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+    name: 'SortToggle',
+
+    props: ['text', 'field', 'sortBy', 'sort'],
+
+    computed: {
+        iconClasses(): object {
+            return {
+                'fa-chevron-up': this.sort === 'ASC',
+                'fa-chevron-down': this.sort === 'DESC'
+            };
+        }
+    },
+
+    methods: {
+        sortColumn() {
+            this.$emit('sortcolumn', this.field);
+        }
+    }
+})
+</script>
