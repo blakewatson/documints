4466b45431d54399784f248a38cf3ff67c0ddd69
Blake Watson <blake@blakewatson.com>
2019-04-28 22:39:40 -0500

    added fake data

--- src/store.ts
+++ src/store.ts
@@ -9,6 +9,7 @@ import {
     NewAccess,
     Uuid
 } from '@/types';
+import fakeApiData from '@/data';
 
 Vue.use(Vuex);
 
@@ -119,25 +120,6 @@ export default new Vuex.Store({
 
         fetchDocuments(context) {
             return new Promise((resolve, reject) => {
-                const fakeApiData: Documint[] = [{
-                    "access": ["joe"],
-                    "id": 1,
-                    "name": "Phylurus milli",
-                    "description": "Vestibulum rutrum rutrum neque.",
-                    "icon": "<i class='fa fa-2x fa-file-powerpoint'></i>",
-                    "expirationDate": "2018/06/15",
-                    "lastViewed": "2017/11/24",
-                    "tags":[]
-                }, {
-                    "access": ["mike"],
-                    "id": 2,
-                    "name": "Paraxerus cepapi",
-                    "description": "Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.",
-                    "icon": "<i class='fa fa-2x fa-file-word'></i>",
-                    "expirationDate": "2018/03/20",
-                    "lastViewed": "2018/03/21",
-                    "tags":["luctus", "lorrem ipsum"]
-                }];
                 resolve(fakeApiData);
             });
         }
